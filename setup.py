0dd8dff1d59035fc93abfe21bd806aea1fb1b3b3
LeraKhabi <96493799+LeraKhabi@users.noreply.github.com>
2021-12-23 05:19:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+NAME = 'vvpd4'
+VERSION = '1.0'
+AUTHORS = 'Valery Khabibrakhmanova'
+
+setup(name=NAME,
+      description='ВВПД4 - ВВПД6',
+      version=VERSION,
+      author=AUTHORS,
+      author_email="khabibrahman.lera@gmail.com",
+      packages=find_packages())
